968c518ecb519cb45b2fb8fc12143ba2aac2ec9a
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-10-20 02:17:19 +0600

    Remove from cart API created

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
       const result = await cartCollection.insertOne(newCart);
       res.send(result);
     });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
