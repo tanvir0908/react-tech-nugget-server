d6151b27172f92d5abaed06ec8767c9571db6c85
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-10-19 23:43:32 +0600

    Updated users collection

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ async function run() {
       const result = await productsCollection.updateOne(filter, product);
       res.send(result);
     });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
