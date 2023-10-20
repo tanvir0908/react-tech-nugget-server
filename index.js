28d9e04cf24bd2a1bd46f70a8a32495fc3578ca8
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-10-21 00:47:09 +0600

    Commented 2 line for vercel deployment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const productsCollection = client.db("techNuggetDB").collection("products");
     const cartCollection = client.db("techNuggetDB").collection("cart");
@@ -92,7 +92,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
